67b5752ec1b184f6a24267ad092e2d9f762b260c
alexmb356 <alexmb356@hotmail.com>
2023-03-26 01:12:04 -0300

    javascript

--- main.js
+++ main.js
@@ -1,29 +1,67 @@
-const input = document.querySelector("input");
+const input = document.querySelector("#tarea");
+const inputDesc = document.querySelector("#message");
 const addBtn = document.querySelector(".btn-add");
-const ul = document.querySelector("ul");
+const section1 = document.querySelector("section1");
 const empty = document.querySelector(".empty");
+const taskCont = document.querySelector("#taskCont");
+const inputColor = document.querySelector("#color")
+const inputDate = document.querySelector("#date")
 
 addBtn.addEventListener("click", (e)=>{
 
     e.preventDefault();
     const text = input.value;
+    const desc = inputDesc.value
+    const color = inputColor.value
+    const date = inputDate.value
 
     if(text !== ""){
-        const li = document.createElement("li");
-        const p = document.createElement("p");
-
-        p.textContent = text;
         
-        li.appendChild(p);
-        li.appendChild(addDeleteBtn());
-        ul.appendChild(li)
+        const div2 = document.createElement("div");
+        const div3 = document.createElement("div");
+        const div4 = document.createElement("div");
+        const p1 = document.createElement("p");
+        const p2 = document.createElement("p");
+        const h4= document.createElement("h4");
+        const li= document.createElement("li");
+        const ul= document.createElement("ul");
+        
+        
 
+        p1.textContent = desc;
+        p2.textContent = date;
+        h4.textContent = text;
+        
+        li.appendChild(p1);
+        li.appendChild(p2);
+        ul.appendChild(li);
+        div4.appendChild(ul);
+        
+        div3.appendChild(h4);
+        div2.appendChild(div3);
+        div2.appendChild(div4);
+        taskCont.appendChild(div2);
+        div2.appendChild(addDeleteBtn());
+
+        p1.className= "card-text"
+        p2.className= "card-text"
+        h4.className="my-0 fw-normal"
+        div3.className= "card-header p-1"
+        div4.className= "card-body p-1 m-1"
+        div2.className= "card border border-light"
+        div2.setAttribute("id","box")
+        div4.setAttribute("id","cardBody")
+        div2.style.backgroundColor= color;
         input.value= "";
-        empty.style.display = "none"
+        inputDesc.value="";
+        empty.style.display = "none";
+        
 
     }
 
 });
+
+
 
 function addDeleteBtn() {
 
@@ -32,13 +70,16 @@ function addDeleteBtn() {
     deleteBtn.className = "btn-delete";
 
     deleteBtn.addEventListener("click", (e)=>{
+        
         const item = e.target.parentElement;
-        ul.removeChild(item);
-    
+        taskCont.removeChild(item)
+        console.log(item) // ulTareas
+        
+
         const items = document.querySelectorAll("li");
-    
         if (items.length === 0) {
           empty.style.display = "block";
+          
         }
     });
     
@@ -47,4 +88,3 @@ function addDeleteBtn() {
     
 
 };
-
