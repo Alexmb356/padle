78deda5373da4a17b83795dcb8e20d98546ac791
alexmb356 <alexmb356@hotmail.com>
2023-03-26 21:57:57 -0300

    Prioridad y Responsive

--- main.js
+++ main.js
@@ -6,6 +6,8 @@ const empty = document.querySelector(".empty");
 const taskCont = document.querySelector("#taskCont");
 const inputColor = document.querySelector("#color")
 const inputDate = document.querySelector("#date")
+const inputTime = document.querySelector("#time")
+const inputUrgencia = document.querySelector("#urgencia")
 
 addBtn.addEventListener("click", (e)=>{
 
@@ -14,15 +16,20 @@ addBtn.addEventListener("click", (e)=>{
     const desc = inputDesc.value
     const color = inputColor.value
     const date = inputDate.value
+    const time = inputTime.value
+    const urgencia = inputUrgencia.value
 
     if(text !== ""){
         
         const div2 = document.createElement("div");
         const div3 = document.createElement("div");
         const div4 = document.createElement("div");
+        const div5 = document.createElement("div");
         const p1 = document.createElement("p");
         const p2 = document.createElement("p");
+        const p3 = document.createElement("p");
         const h4= document.createElement("h4");
+        const h5= document.createElement("h5");
         const li= document.createElement("li");
         const ul= document.createElement("ul");
         
@@ -30,10 +37,13 @@ addBtn.addEventListener("click", (e)=>{
 
         p1.textContent = desc;
         p2.textContent = date;
+        p3.textContent = time;
         h4.textContent = text;
+        h5.textContent = urgencia;
         
         li.appendChild(p1);
         li.appendChild(p2);
+        li.appendChild(p3);
         ul.appendChild(li);
         div4.appendChild(ul);
         
@@ -41,7 +51,20 @@ addBtn.addEventListener("click", (e)=>{
         div2.appendChild(div3);
         div2.appendChild(div4);
         taskCont.appendChild(div2);
+        div5.appendChild(h5);
+        div2.appendChild(div5);
         div2.appendChild(addDeleteBtn());
+
+        console.log(urgencia)
+        if(urgencia== "Alta"){
+            div5.style.backgroundColor= 'red';
+        
+        }
+        else if (urgencia=="Media") {
+            div5.style.backgroundColor= "#FFFF00";
+        } else {
+            div5.style.backgroundColor= "#0000FF";
+        }
 
         p1.className= "card-text"
         p2.className= "card-text"
